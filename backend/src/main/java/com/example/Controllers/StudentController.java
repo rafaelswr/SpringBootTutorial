be217fb3810f0baae109487e89c6953bfef485ef
rafaelswr <rafaels.borges91@gmail.com>
2023-10-13 09:05:12 +0100

    Set Dependency Injection
    Dependency injection on StudentService in StudentController constructor

--- backend/src/main/java/com/example/Controllers/StudentController.java
+++ backend/src/main/java/com/example/Controllers/StudentController.java
@@ -17,6 +17,7 @@ public class StudentController {
 
     private final StudentService studentService;
 
+    @Autowired
     public StudentController(StudentService studentService){
         this.studentService = studentService;
     }
