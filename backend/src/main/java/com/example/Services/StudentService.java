f23cd72b2dc973e0b84c65f845f99caaa5e7bfce
rafaelswr <rafaels.borges91@gmail.com>
2023-10-15 16:07:34 +0100

    Student Repository
    Created Student Repository. In studentService, was declared the repository and assigned autowired.

--- backend/src/main/java/com/example/Services/StudentService.java
+++ backend/src/main/java/com/example/Services/StudentService.java
@@ -1,6 +1,8 @@
 package com.example.Services;
 
 import com.example.Models.Student;
+import com.example.Repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,8 +10,15 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return List.of(new Student(), new Student());
+        return studentRepository.findAll();
     }
 
 }
