6e5dc0eb60192fc2434a6b2515be73a4457dc2c9
rafaelswr <rafaels.borges91@gmail.com>
2023-10-15 15:44:59 +0100

    Model Student, add SequenceGenerator and GeneratedValue
    Model Student, add SequenceGenerator and GeneratedValue

--- backend/src/main/java/com/example/Models/Student.java
+++ backend/src/main/java/com/example/Models/Student.java
@@ -1,19 +1,31 @@
 package com.example.Models;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
 
-@Data
+
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "student")
 public class Student {
 
-
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
+
     private String name;
     private String email;
     private int age;
